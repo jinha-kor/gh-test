674f6f028edf433b454b9c15eeefe575306fc806
jinha <jinha880311@gmail.com>
2022-12-29 13:22:41 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+const repository = "haruCode";
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: !debug ? "/${repository}/" : "/",
+  trailingSlash: true,
+  images: {
+    loader: "imgix",
+    path: "https://jinha0311.github.io",
+  },
 }
 
 module.exports = nextConfig
