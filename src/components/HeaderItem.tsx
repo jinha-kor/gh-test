716b716ef8827a914561cd56701522180c87e7f1
jinha <jinha880311@gmail.com>
2023-01-02 17:01:43 +0900

    init

--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 const HeaderItem: React.FC<Props> = (props) => {
     const defaultProps: Props = {
+        id: '',
         Icon: null,
         title: '',
         flag: 'sub',
@@ -18,12 +19,12 @@ const HeaderItem: React.FC<Props> = (props) => {
             {props.flag === 'main' ? (
                 <span className="headerSpan" onClick={itemClickHandler} >
                     <props.Icon className="h-20" />
-                    <p className="text-7xl font-extrabold" >{props.title}</p>
+                    <p id={props.id} className="text-7xl font-extrabold" >{props.title}</p>
                 </span>
             ) : (
                 <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white">
                     <props.Icon className="h-8 mb-1 group-hover:animate-bounce"/>
-                    <p className="opacity-0 group-hover:opacity-100 tracking-widest">{props.title}</p>
+                    <p id={props.id} className="opacity-0 group-hover:opacity-100 tracking-widest">{props.title}</p>
                 </div>
             )}
         </>
@@ -31,6 +32,7 @@ const HeaderItem: React.FC<Props> = (props) => {
 };
 
 export interface Props {
+    id?: string,
     Icon: any,
     title: string,
     flag?: string,
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -7,17 +7,21 @@ const Header = () => {
     const router = useRouter();
 
     const iconClickHandler = useCallback((e:any) => {
-        console.log(e);
-    }, []);
+        const { id } = e.target;
+        let url = '/';
+        if (id && id !== 'main') url += `${id}`;
+
+        router.push(url);
+    }, [router]);
 
     return (
         <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
             <div className="flex cursor-pointer transform hover:scale-105">
-                <HeaderItem flag="main" title='TEST' Icon={Icons.CodeBracketIcon} />
+                <HeaderItem id="main" flag="main" title='TEST' Icon={Icons.CodeBracketIcon} onClick={iconClickHandler} />
             </div>
             <div className="flex flex-grow justify-evenly max-w-sm">
-                <HeaderItem flag="sub" title='CUBE' Icon={Icons.CubeIcon} onClick={iconClickHandler} />
-                <HeaderItem flag="sub" title='ACCOUNT' Icon={Icons.UserIcon} onClick={iconClickHandler} />
+                <HeaderItem id="cube" flag="sub" title='CUBE' Icon={Icons.CubeIcon} onClick={iconClickHandler} />
+                <HeaderItem id="account" flag="sub" title='ACCOUNT' Icon={Icons.UserIcon} onClick={iconClickHandler} />
             </div>
         </header>
     )
