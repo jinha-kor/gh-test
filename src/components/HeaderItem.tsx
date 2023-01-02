c2184b62855238290237ee02e022b06324bcd745
jinha <jinha880311@gmail.com>
2023-01-02 17:52:34 +0900

    init

--- src/components/HeaderItem.tsx
+++ src/components/HeaderItem.tsx
@@ -17,12 +17,14 @@ const HeaderItem: React.FC<Props> = (props) => {
     return (
         <>
             {props.flag === 'main' ? (
-                <span className="headerSpan" onClick={itemClickHandler} >
-                    <props.Icon id={props.id} className="h-20" />
-                    <p id={props.id} className="text-7xl font-extrabold" >{props.title}</p>
-                </span>
+                <div id={props.id} className="flex cursor-pointer transform hover:scale-105" onClick={itemClickHandler}>
+                    {/*<span id={props.id} className="headerSpan" onClick={itemClickHandler} >*/}
+                        <props.Icon id={props.id} className="h-20" />
+                        <p id={props.id} className="text-7xl font-extrabold" >{props.title}</p>
+                    {/*</span>*/}
+                </div>
             ) : (
-                <div className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white" onClick={itemClickHandler} >
+                <div id={props.id} className="flex flex-col items-center cursor-pointer group w-12 sm:w-20 hover:text-white" onClick={itemClickHandler} >
                     <props.Icon id={props.id} className="h-8 mb-1 group-hover:animate-bounce"/>
                     <p id={props.id} className="opacity-0 group-hover:opacity-100 tracking-widest">{props.title}</p>
                 </div>
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -7,18 +7,18 @@ const Header = () => {
     const router = useRouter();
 
     const iconClickHandler = useCallback((e:any) => {
-        const { id } = e.target;
-        let url = '/';
-        if (id && id !== 'main') url += `${id}`;
+        const { id } = e.target.parentNode;
 
-        router.push(url);
+        if (id) {
+            let url = '/';
+            if (id !== 'main') url += `${id}`;
+            router.push(url);
+        }
     }, [router]);
 
     return (
         <header className="flex flex-col sm:flex-row m-5 justify-between items-center">
-            <div className="flex cursor-pointer transform hover:scale-105">
-                <HeaderItem id="main" flag="main" title='TEST' Icon={Icons.HomeIcon} onClick={iconClickHandler} />
-            </div>
+            <HeaderItem id="main" flag="main" title='TEST' Icon={Icons.HomeIcon} onClick={iconClickHandler} />
             <div className="flex flex-grow justify-evenly max-w-sm">
                 <HeaderItem id="about" flag="sub" title='ABOUT' Icon={Icons.UserIcon} onClick={iconClickHandler} />
                 <HeaderItem id="skill" flag="sub" title='SKILL' Icon={Icons.CodeBracketIcon} onClick={iconClickHandler} />
