0b74875b1e813f0912ee5348c735b6ec40a9a29d
jinha <jinha880311@gmail.com>
2023-01-02 14:00:30 +0900

    init

--- next.config.js
+++ next.config.js
@@ -1,12 +1,11 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const isProd = process.env.NODE_ENV === "production";
-const repository = "blog";
+const repository = "https://jinha-kor.github.io";
 
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: isProd ? `/${repository}` : '',
-  basePath: isProd ? `/${repository}` : '',
+  assetPrefix: isProd ? `${repository}/` : '',
   trailingSlash: true,
   images: {
     loader: 'akamai',
