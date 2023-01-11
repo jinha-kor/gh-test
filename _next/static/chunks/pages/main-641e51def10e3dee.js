9aafc896e98e833b32b3a8d8e1ce0b978145cf06
jinha <jinha880311@gmail.com>
2023-01-11 17:16:19 +0900

    Updates

---
+++ _next/static/chunks/pages/main-641e51def10e3dee.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[671],{5206:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/main",function(){return u(29)}])}},function(n){n.O(0,[675,29,774,888,179],function(){return n(n.s=5206)}),_N_E=n.O()}]);
---
+++ skillMockup.ts
@@ -0,0 +1,52 @@
+export const frontImgList = [
+    [
+        { url: '/images/skills/javascript.jpg', alt: 'javascript', idx: 0 },
+        { url: '/images/skills/jquery.png', alt: 'jquery', idx: 1 },
+        { url: '/images/skills/react.png', alt: 'react', idx: 2 },
+        { url: '/images/skills/nextjs.png', alt: 'nextjs', idx: 3 },
+        { url: '/images/skills/redux.png', alt: 'redux', idx: 4 },
+    ],
+    [
+        { url: '/images/skills/bootstrap.png', alt: 'bootstrap', idx: 5 },
+        { url: '/images/skills/typescript.png', alt: 'typescript', idx: 6 },
+        {
+            url: '/images/skills/tailwindcss.png',
+            alt: 'tailwindcss',
+            idx: 7,
+        },
+    ],
+];
+
+export const backImgList = [
+    [
+        { url: '/images/skills/spring.png', alt: 'spring', idx: 1 },
+        { url: '/images/skills/gradle.png', alt: 'gradle', idx: 2 },
+        { url: '/images/skills/kotlin.png', alt: 'kotlin', idx: 3 },
+    ],
+];
+
+export const dbImgList = [
+    [
+        { url: '/images/skills/oracle.png', alt: 'oracle', idx: 0 },
+        { url: '/images/skills/postgrssql.png', alt: 'postgrssql', idx: 1 },
+        { url: '/images/skills/tibero.jpg', alt: 'tibero', idx: 2 },
+    ],
+];
+
+export const vccImgList = [
+    [
+        { url: '/images/skills/jira.png', alt: 'jira', idx: 0 },
+        { url: '/images/skills/confluence.jpg', alt: 'confluence', idx: 1 },
+        { url: '/images/skills/git.png', alt: 'git', idx: 2 },
+        { url: '/images/skills/github.png', alt: 'github', idx: 3 },
+    ],
+];
+
+export const toolImgList = [
+    [
+        { url: '/images/skills/vscode.png', alt: 'vscode', idx: 0 },
+        { url: '/images/skills/eclipse.jpg', alt: 'eclipse', idx: 1 },
+        { url: '/images/skills/intellij.png', alt: 'intellij', idx: 2 },
+        { url: '/images/skills/nexacro.jpg', alt: 'nexacro', idx: 3 },
+    ],
+];
